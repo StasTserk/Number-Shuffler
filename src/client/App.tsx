47d6476cab617bb2a280f8bb2ec24a1be352fd38
Stas Tserkovny <stas.tserkovny@gmail.com>
2022-07-29 18:22:07 -0600

    Add MUI and basic page layout
    Add MUI to the project
    Add basic page layout for displaying and loading numbers.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,18 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import * as React from 'react';
+import { Header } from './components/Header';
+import { NumberPicker } from './components/NumberPicker';
+import { NumberViewer } from './components/NumberViewer';
 
 export const App = (): JSX.Element => {
+    const numbers = Array(1000)
+        .fill(0)
+        .map((_, i) => i);
     return (
         <Box>
-            <Typography variant="body1">Hello World</Typography>
+            <Header />
+            <NumberPicker />
+            <NumberViewer numbers={numbers} />
         </Box>
     );
 };
---
+++ src/client/components/NumberViewer.tsx
@@ -0,0 +1,39 @@
+import { Box, Paper, SxProps, Typography } from '@mui/material';
+import * as React from 'react';
+
+const viewerStyle: SxProps = {
+    p: 2,
+    my: 2,
+    overflowY: 'auto',
+    maxHeight: 'calc(100vh - 208px)',
+};
+const numberGridStyle: SxProps = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(80px, 1fr))',
+    gap: 2,
+};
+
+type NumberViewerProps = {
+    numbers: number[];
+};
+
+const NumberItem = ({ number }: { number: number }): JSX.Element => {
+    return (
+        <Paper variant="outlined" square sx={{ p: 2 }}>
+            {number}
+        </Paper>
+    );
+};
+
+export const NumberViewer = ({ numbers }: NumberViewerProps): JSX.Element => {
+    return (
+        <Paper sx={viewerStyle} elevation={2}>
+            <Typography variant="body1">Randomly Generated Numbers:</Typography>
+            <Box sx={numberGridStyle}>
+                {numbers.map((n) => (
+                    <NumberItem number={n} key={n} />
+                ))}
+            </Box>
+        </Paper>
+    );
+};
