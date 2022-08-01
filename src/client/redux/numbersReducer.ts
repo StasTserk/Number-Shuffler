3e8b2cc8225d7e9233f3303c3c7bd119c2706820
Stas Tserkovny <stas.tserkovny@gmail.com>
2022-07-31 18:15:33 -0600

    Connect numbersReducer to fetchShuffled thunk

--- src/client/redux/numbersReducer.ts
+++ src/client/redux/numbersReducer.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { loadShuffled } from './loadShuffled';
 
 type NumbersState = {
     numbers: number[];
@@ -15,4 +16,18 @@ export const numberSlice = createSlice({
     name: 'numbers',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(loadShuffled.pending, (state) => {
+            state.status = 'loading';
+            state.error = undefined;
+        });
+        builder.addCase(loadShuffled.rejected, (state, { payload }) => {
+            state.status = 'error';
+            state.error = payload;
+        });
+        builder.addCase(loadShuffled.fulfilled, (state, { payload }) => {
+            state.status = 'ready';
+            state.numbers = payload;
+        });
+    },
 });
