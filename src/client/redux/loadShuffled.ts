a71b2b8d73013e8ea427aebf69e4c6dc587cdc68
Stas Tserkovny <stas.tserkovny@gmail.com>
2022-07-31 18:15:01 -0600

    Rename fetchNumbers to loadShuffled to match thunk

--- src/client/redux/loadShuffled.ts
+++ src/client/redux/loadShuffled.ts
@@ -10,5 +10,8 @@ export const loadShuffled = createAsyncThunk<
     const numbers = Array(numberNeeded)
         .fill(0)
         .map((_, i) => i);
+    if (Math.random() > 0.5) {
+        return rejectWithValue('Randomly decided to fail');
+    }
     return Promise.resolve(numbers);
 });
