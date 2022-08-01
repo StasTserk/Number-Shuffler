17fdbda005ed8f2155c2beab0f56693f7849ecb9
Stas Tserkovny <stas.tserkovny@gmail.com>
2022-07-31 18:58:09 -0600

    Connect NumberPicker to redux
    Track amount of numbers we want to load
    Connect numberPicker to redux store

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 import * as React from 'react';
 import { useEffect } from 'react';
 import { Header } from './components/Header';
-import { NumberPicker } from './components/NumberPicker';
+import NumberPicker from './components/NumberPicker';
 import NumberViewer from './components/NumberViewer';
 import { numberActions } from './redux/store';
 
